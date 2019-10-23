a7860b602c8642f05807c56070ebc512a5f7882a
jprolejko <jprolejko@gmail.com>
2019-10-23 23:16:46 +0200

    Added switching between weapons by scrolling

--- src/objects/Player.cpp
+++ src/objects/Player.cpp
@@ -15,12 +15,17 @@ Player::Player(const sf::Vector2f &position,
                       velocity,
                       {SIZE_X_, SIZE_Y_},
                       "player",
-                      CFG.getFloat("player_max_acceleration")),
-        weapon_(ResourceManager::getInstance().getWeapon("rocket_launcher")) {
+                      CFG.getFloat("player_max_acceleration")) {
+    weapons_in_backpack_.emplace_back(ResourceManager::getInstance().getWeapon("m4"));
+    weapons_in_backpack_.emplace_back(ResourceManager::getInstance().getWeapon("desert_eagle"));
+    weapons_in_backpack_.emplace_back(ResourceManager::getInstance().getWeapon("shotgun"));
+    weapons_in_backpack_.emplace_back(ResourceManager::getInstance().getWeapon("rocket_launcher"));
+
+    current_weapon_ = weapons_in_backpack_.begin();
 }
 
 void Player::shot() {
-    auto new_velocity = weapon_.shot();
+    auto new_velocity = current_weapon_->shot();
 
     if (!utils::isNearlyEqual(new_velocity, {0.0f, 0.0f}))
     {
@@ -29,12 +34,25 @@ void Player::shot() {
     }
 }
 
+void Player::switchWeapon(int relative_position_backpack) {
+    current_weapon_ = current_weapon_ + relative_position_backpack;
+
+    if (current_weapon_ >= weapons_in_backpack_.end())
+    {
+        current_weapon_ = weapons_in_backpack_.begin();
+    }
+
+    if (current_weapon_ < weapons_in_backpack_.begin())
+    {
+        current_weapon_ = weapons_in_backpack_.end() - 1;
+    }
+}
 
 void Player::update(float time_elapsed) {
     DynamicObject::update(time_elapsed);
 
-    weapon_.setPosition(this->getPosition());
-    weapon_.setRotation(this->getRotation());
+    current_weapon_->setPosition(this->getPosition());
+    current_weapon_->setRotation(this->getRotation());
 }
 
 void Player::draw(sf::RenderTarget &target, sf::RenderStates states) const {
@@ -70,6 +88,6 @@ void Player::draw(sf::RenderTarget &target, sf::RenderStates states) const {
             target.draw(&trail_vert[0], trail_vert.size(), sf::TriangleStrip, states);
         }
         target.draw(shape_, states);
-        target.draw(weapon_, states);
+        target.draw(*current_weapon_, states);
     }
 }

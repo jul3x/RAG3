191a571c90cd588d2c769f2b3089c1310edc1b22
Julian Prolejko <jprolejko@gmail.com>
2020-04-02 11:02:02 +0200

    PoC of thoughts

--- game/include/misc/Thought.h
+++ game/include/misc/Thought.h
@@ -5,6 +5,8 @@
 #ifndef RAG3_GAME_MISC_THOUGHT_H
 #define RAG3_GAME_MISC_THOUGHT_H
 
+#include <list>
+
 #include <SFML/Graphics/Text.hpp>
 
 #include <R3E/objects/AbstractPhysicalObject.h>
@@ -18,6 +20,15 @@ public:
 
     bool update(float time_elapsed) override;
 private:
+    static constexpr float SIZE_X_ = 200.0f;
+    static constexpr float SIZE_Y_BOTTOM_ = 50.0f;
+    static constexpr float SIZE_Y_CENTER_ = 25.0f;
+    static constexpr float SIZE_Y_TOP_ = 25.0f;
+    static constexpr float GLOBAL_OFFSET_X_ = 25.0f;
+    static constexpr float GLOBAL_OFFSET_Y_ = -50.0f;
+    static constexpr float TEXT_MARGIN_ = 10.0f;
+    static constexpr int TEXT_SIZE_ = 16;
+
     void draw(sf::RenderTarget& target, sf::RenderStates states) const override;
 
     float time_elapsed_;
@@ -25,6 +36,8 @@ private:
     sf::Text text_;
 
     AbstractPhysicalObject* father_;
+    AbstractDrawableObject top_;
+    std::vector<AbstractDrawableObject> center_;
 
 };
 
--- game/src/misc/Thought.cpp
+++ game/src/misc/Thought.cpp
@@ -9,21 +9,50 @@
 
 Thought::Thought(AbstractPhysicalObject* father, const std::string& text, float duration) :
         father_(father),
-        text_(text, RM.getFont(), 24),
+        text_(text, RM.getFont(), TEXT_SIZE_),
         time_elapsed_(duration),
-        AbstractPhysicalObject(father->getPosition(), {1.0f, 1.0f}, Collision::None(), nullptr)
+        AbstractPhysicalObject(father->getPosition() + sf::Vector2f{GLOBAL_OFFSET_X_, GLOBAL_OFFSET_Y_}, {SIZE_X_, SIZE_Y_BOTTOM_}, Collision::None(), &RM.getTexture("thought_bottom")),
+        top_(father->getPosition() + sf::Vector2f{GLOBAL_OFFSET_X_, GLOBAL_OFFSET_Y_}, {SIZE_X_, SIZE_Y_TOP_}, &RM.getTexture("thought_top"))
 {
+    this->changeOrigin({0.0f, SIZE_Y_BOTTOM_});
+    top_.changeOrigin({0.0f, SIZE_Y_TOP_});
+
+    text_.setFillColor(sf::Color::Black);
+
+    auto count = std::count(text.begin(), text.end(), '\n');
+    center_.reserve(count);
+    for (size_t i = 0; i < count; ++i)
+    {
+        center_.emplace_back(father->getPosition() + sf::Vector2f{GLOBAL_OFFSET_X_, GLOBAL_OFFSET_Y_ - SIZE_Y_BOTTOM_ - i * SIZE_Y_CENTER_}, sf::Vector2f{SIZE_X_, SIZE_Y_CENTER_}, &RM.getTexture("thought"));
+        center_.at(i).changeOrigin({0.0f, SIZE_Y_CENTER_});
+    }
+
+    top_.setPosition(father->getPosition() + sf::Vector2f{GLOBAL_OFFSET_X_, GLOBAL_OFFSET_Y_ - count * SIZE_Y_CENTER_ - SIZE_Y_BOTTOM_});
 }
 
 bool Thought::update(float time_elapsed)
 {
     time_elapsed_ -= time_elapsed;
-    this->setPosition(father_->getPosition());
-    text_.setPosition(father_->getPosition());
+    this->setPosition(father_->getPosition() + sf::Vector2f{GLOBAL_OFFSET_X_, GLOBAL_OFFSET_Y_});
+
+    for (size_t i = 0; i < center_.size(); ++i)
+    {
+        center_.at(i).setPosition(father_->getPosition() + sf::Vector2f{GLOBAL_OFFSET_X_, GLOBAL_OFFSET_Y_ - SIZE_Y_BOTTOM_ - i * SIZE_Y_CENTER_});
+    }
+
+    top_.setPosition(father_->getPosition() + sf::Vector2f{GLOBAL_OFFSET_X_, GLOBAL_OFFSET_Y_ - center_.size() * SIZE_Y_CENTER_ - SIZE_Y_BOTTOM_});
+    text_.setPosition(father_->getPosition() + sf::Vector2f{GLOBAL_OFFSET_X_ + TEXT_MARGIN_, GLOBAL_OFFSET_Y_ - center_.size() * SIZE_Y_CENTER_ - SIZE_Y_BOTTOM_ + SIZE_Y_TOP_ / 2.0f - TEXT_SIZE_});
+
     return time_elapsed_ > 0.0f;
 }
 
 void Thought::draw(sf::RenderTarget& target, sf::RenderStates states) const
 {
+    target.draw(shape_, states);
+
+    for (auto& center : center_)
+        target.draw(center, states);
+
+    target.draw(top_, states);
     target.draw(text_, states);
 }

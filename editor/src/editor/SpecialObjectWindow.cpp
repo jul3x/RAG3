0daa016b9e226025ae2589a5ed05ee194f2bb24b
jul3x <jprolejko@gmail.com>
2021-10-28 21:29:46 +0200

    Special Functions editor PoC

--- editor/src/editor/SpecialObjectWindow.cpp
+++ editor/src/editor/SpecialObjectWindow.cpp
@@ -15,7 +15,8 @@ SpecialObjectWindow::SpecialObjectWindow(tgui::Gui* gui, tgui::Theme* theme) :
                      CONF<sf::Vector2f>("popup_window_size")) / 2.0f,
                     CONF<sf::Vector2f>("popup_window_size"),
                     "special_object_window"),
-        special_(nullptr)
+        special_(nullptr),
+        functions_(theme)
 {
     grid_ = tgui::Grid::create();
     grid_->setPosition("50% - width/2", "20");
@@ -42,39 +43,13 @@ SpecialObjectWindow::SpecialObjectWindow(tgui::Gui* gui, tgui::Theme* theme) :
     label->setText("Activate:");
     label->setTextSize(CONF<float>("label_text_size"));
 
-    grid_->addWidget(label, 0, 1);
+    grid_->addWidget(label, 2, 0);
 
     act_box_ = tgui::EditBox::create();
     act_box_->setRenderer(theme_->getRenderer("EditBox"));
     act_box_->setSize("35%", CONF<float>("text_box_height"));
     act_box_->setTextSize(CONF<float>("label_text_size"));
-    grid_->addWidget(act_box_, 1, 1);
-
-    label = tgui::Label::create();
-    label->setRenderer(theme_->getRenderer("Label"));
-    label->setText("Function:");
-    label->setTextSize(CONF<float>("label_text_size"));
-
-    grid_->addWidget(label, 2, 0);
-
-    fun_box_ = tgui::TextBox::create();
-    fun_box_->setRenderer(theme_->getRenderer("TextBox"));
-    fun_box_->setSize("35%", "35%");
-    fun_box_->setTextSize(CONF<float>("label_text_size"));
-    grid_->addWidget(fun_box_, 3, 0);
-
-    label = tgui::Label::create();
-    label->setRenderer(theme_->getRenderer("Label"));
-    label->setText("Function data:");
-    label->setTextSize(CONF<float>("label_text_size"));
-
-    grid_->addWidget(label, 2, 1);
-
-    data_box_ = tgui::TextBox::create();
-    data_box_->setRenderer(theme_->getRenderer("TextBox"));
-    data_box_->setSize("35%", "35%");
-    data_box_->setTextSize(CONF<float>("label_text_size"));
-    grid_->addWidget(data_box_, 3, 1);
+    grid_->addWidget(act_box_, 3, 0);
 
     label = tgui::Label::create();
     label->setRenderer(theme_->getRenderer("Label"));
@@ -88,6 +63,8 @@ SpecialObjectWindow::SpecialObjectWindow(tgui::Gui* gui, tgui::Theme* theme) :
     already_active_->setTextSize(CONF<float>("label_text_size"));
     grid_->addWidget(already_active_, 5, 0);
 
+    grid_->addWidget(functions_.getGrid(), 6, 0);
+
     button_ = tgui::Button::create();
     button_->setRenderer(theme_->getRenderer("Button"));
     button_->setText("Save");
@@ -95,14 +72,7 @@ SpecialObjectWindow::SpecialObjectWindow(tgui::Gui* gui, tgui::Theme* theme) :
     button_->setSize(CONF<sf::Vector2f>("button_size"));
     button_->setPosition("50% - width/2", "100% - " + std::to_string(CONF<float>("button_relative_valign")));
 
-    child_->add(button_);
-
-    float padding = CONF<float>("items_padding");
-    grid_->setWidgetPadding(1, 0, {padding, padding});
-    grid_->setWidgetPadding(1, 1, {padding, padding});
-    grid_->setWidgetPadding(3, 0, {padding, padding});
-    grid_->setWidgetPadding(3, 1, {padding, padding});
-    grid_->setWidgetPadding(5, 0, {padding, padding});
+    grid_->addWidget(button_, 7, 0);
 }
 
 void SpecialObjectWindow::setObjectContent(const std::string& category, Special* obj)
@@ -111,14 +81,14 @@ void SpecialObjectWindow::setObjectContent(const std::string& category, Special*
     child_->setTitle(category + "/" + special_->getId());
     id_box_->setText(std::to_string(special_->getUniqueId()));
     act_box_->setText(special_->getActivationStr());
-    fun_box_->setText(special_->getFunctionsStr());
-    data_box_->setText(special_->getDatasStr());
     already_active_->setText(std::to_string(special_->isActive()));
+    functions_.setObjectContent(obj);
 
     button_->connect("pressed", [this]() {
         this->special_->setActivationStr(this->act_box_->getText());
-        this->special_->setFunctionsStr(this->fun_box_->getText());
-        this->special_->setDatasStr(this->data_box_->getText());
+
+        this->special_->setFunctions(this->functions_.getFunctions());
+        this->special_->setDatas(this->functions_.getDatas());
 
         if (already_active_->getText() == "1")
         {
@@ -135,12 +105,13 @@ void SpecialObjectWindow::setObjectContent(const std::string& category, Special*
 
 bool SpecialObjectWindow::isDataFocused() const
 {
-    return data_box_->isFocused();
+//    return data_box_->isFocused();
+    return false;
 }
 
 void SpecialObjectWindow::addToData(const std::string& str)
 {
-    data_box_->setText(data_box_->getText() + str);
+//    data_box_->setText(data_box_->getText() + str);
 }
 
 
